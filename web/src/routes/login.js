820a86d82b4954e95c4cf1e10b36b30e7fc99ea9
JTRB <jacobrobertsbaca@gmail.com>
2022-05-03 13:23:23 -0700

    feat: add basic authentication through AWS

--- web/src/routes/login.js
+++ web/src/routes/login.js
@@ -1,13 +1,31 @@
+import { Auth } from 'aws-amplify';
+import { Formik, Form } from 'formik';
+import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
+import { bool, object, string } from 'yup';
+import LabelInput from '../components/form/LabelInput';
+import Switch from '../components/form/Switch';
 import Page from '../components/Page';
+
+const LoginSchema = object({
+    email: string().email('Enter a valid email').required('Required!'),
+    password: string().required('Required!'),
+    rememberMe: bool()
+});
 
 export default function Login() {
     const navigate = useNavigate();
 
-    const submit = evt => {
-        navigate('/dashboard');
-        evt.preventDefault();
-        return false; 
+    const submit = async (values, { setSubmitting }) => {
+        // Small pause for effect
+        await new Promise(r => setTimeout(r, 200));
+        try {
+            await Auth.signIn(values.email, values.password);
+            navigate('/dashboard');
+        } catch (error) {
+            toast.error(`Failed to log in: ${error.message}`)
+        }
+        setSubmitting(false);
     }
 
     return (
@@ -16,17 +34,20 @@ export default function Login() {
                 <h1 className="title">splitr</h1>
                 <h2>A web-app for sharing expenses among friends.</h2>
             </hgroup>
-            <form onSubmit={submit}>
-                <input type="text" name="login" placeholder="Email" aria-label="Login" autoComplete="nickname" required />
-                <input type="password" name="password" placeholder="Password" aria-label="Password" autoComplete="current-password" required />
-                <fieldset>
-                    <label htmlFor="remember">
-                        <input type="checkbox" role="switch" id="remember" name="remember" />
-                        Remember me
-                    </label>
-                </fieldset>
-                <button type="submit" className="contrast">Login</button>
-            </form>
+            <Formik
+                initialValues={{ email: '', password: '', rememberMe: false }}
+                validationSchema={LoginSchema}
+                onSubmit={submit}
+            >
+                {({ isSubmitting }) => (
+                    <Form noValidate>
+                        <LabelInput type='text' label='Email' name='email' placeholder='Email' autoComplete='email' />
+                        <LabelInput type='password' label='Password' name='password' placeholder='Password' autoComplete='password' />
+                        <Switch name='rememberMe' label='Remember Me' />
+                        <button type="submit" className="contrast" disabled={isSubmitting} aria-busy={isSubmitting}>Login</button>
+                    </Form>
+                )}
+            </Formik>
         </Page>
     );
 }

87f88bd61d819b7e7ca70d87ae7cbced83efa3a8
JTRB <jacobrobertsbaca@gmail.com>
2022-05-02 22:46:55 -0700

    feat: add aws amplify

--- web/src/index.js
+++ web/src/index.js
@@ -5,6 +5,10 @@ import { HashRouter } from 'react-router-dom';
 import './index.scss';
 import Modal from 'react-modal';
 
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+
+Amplify.configure(awsExports);
 Modal.setAppElement('#root');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<HashRouter><App /></HashRouter>);
